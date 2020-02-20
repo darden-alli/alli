7089b1e5331eb3c7a40dbb191ddb153993783612
James Henderson <james@identyti.io>
2020-02-20 01:32:05 -0500

    test paths

---
+++ frontend/frontend/src/components/Confidential.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Confidential() {
+  return(
+    <main>
+      <d1>Confidential Page</d1>
+    </main>
+  )
+}
---
+++ frontend/frontend/src/components/Resources.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Resources() {
+  return(
+    <main>
+      <d1>Resources Page</d1>
+    </main>
+  )
+}
